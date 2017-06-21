8e72033b87df33668c233a00bc4c49ed1617bdc5
Quan Jin <quan.jin@me.com>
2017-06-21 09:43:24 -0500

    Initial commit

---
+++ src/main/java/design/hustlelikeaboss/customr/CustomrApplication.java
@@ -0,0 +1,12 @@
+package design.hustlelikeaboss.customr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomrApplication.class, args);
+	}
+}
